993bcb790da9b40c210fda9e5f14ef504a213fa2
Fernando-Linhares <fernandomadethat@gmail.com>
2022-03-23 21:49:27 -0300

    added functionalities to register data users on database

--- src/Controller/SendContactController.php
+++ src/Controller/SendContactController.php
@@ -2,19 +2,49 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use DateTimeImmutable;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class SendContactController extends AbstractController
 {
-    #[Route('/send/contact', methods: 'POST', name: 'app_send_contact')]
-    public function index(): Response
+    #[Route('/send/contact', methods: 'POST', name: 'send_contact')]
+    public function store(Request $request, ManagerRegistry $managerRegistry): Response
     {
+        $manager  = $managerRegistry->getManager();
+
+        $user = new User;
+        $user->setName($request->get('name'));
+        $user->setEmail($request->get('email'));
+        $user->setPhone($request->get('phone'));
+        $user->setCep($request->get('cep'));
+        $user->setUpdatedAt(new DateTimeImmutable());
+        $user->setCreatedAt(new DateTimeImmutable());
+        $manager->persist($user);
+        $manager->flush();
 
         return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/SendContactController.php',
+            'message' => 'user inserted successfully'
+        ]);
+    }
+
+    #[Route('/list/contact', methods: 'GET', name: 'list_contact')]
+    public function list(ManagerRegistry $managerRegistry)
+    {        
+        $allContacts = $managerRegistry->getRepository(User::class)->findAll();
+
+        $allContactSerialize = array_map(function($contact){
+            return $contact->toArray();
+        }, $allContacts);
+       
+        return $this->json([
+            'data'=> $allContactSerialize
         ]);
     }
 }
---
+++ src/Entity/User.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\Table(name: '`user`')]
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $name;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $email;
+
+    #[ORM\Column(type: 'integer')]
+    private $phone;
+
+    #[ORM\Column(type: 'integer')]
+    private $cep;
+
+    #[ORM\Column(type: 'datetime_immutable')]
+    private $updated_at;
+
+    #[ORM\Column(type: 'datetime_immutable')]
+    private $created_at;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getPhone(): ?int
+    {
+        return $this->phone;
+    }
+
+    public function setPhone(int $phone): self
+    {
+        $this->phone = $phone;
+
+        return $this;
+    }
+
+    public function getCep(): ?int
+    {
+        return $this->cep;
+    }
+
+    public function setCep(int $cep): self
+    {
+        $this->cep = $cep;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updated_at;
+    }
+
+    public function setUpdatedAt(\DateTimeImmutable $updated_at): self
+    {
+        $this->updated_at = $updated_at;
+
+        return $this;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $created_at): self
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->created_at;
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'name'       =>  $this->getName(),
+            'email'      =>  $this->getEmail(),
+            'created_at' =>  $this->getCreatedAt()
+        ];
+    }
+}
